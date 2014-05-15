35948c5cc10af3ed1c2745fa53818492ed8c5225
eliotberriot <contact@eliotberriot.com>
2014-05-16 01:50:11 +0200

    changes to wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,1 @@
-from screensizer import app, settings
-
-if __name__ == "__main__":
-    app.run(host=settings.hostname, port=settings.port, debug=settings.debug)
+from screensizer import app as application
