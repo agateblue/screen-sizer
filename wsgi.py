6d5a60e5b2f4548dee4b46df269746d90f6f816d
eliotberriot <contact@eliotberriot.com>
2014-05-16 02:16:37 +0200

    Fixed bug in wsgi.py file

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,12 @@
 import sys
-execfile(activate_this, dict(__file__=activate_this))
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 
 import settings
 import os
 
  # from http://bucksnort.pennington.net/blog/post/deploy-flask-mod_wsgi_virtenv/
 activate_this = settings.virtualenv_path + '/bin/activate_this.py'
-
-sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
+execfile(activate_this, dict(__file__=activate_this))
 
 from screensizer import app as application
 
