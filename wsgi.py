7380d8ae24ac7e65fd3c33c1a23537e59cf52e0b
eliotberriot <contact@eliotberriot.com>
2014-05-16 02:14:56 +0200

    Fixed bug in wsgi.py file

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,11 @@
+import sys
+execfile(activate_this, dict(__file__=activate_this))
+
 import settings
 import os
 
  # from http://bucksnort.pennington.net/blog/post/deploy-flask-mod_wsgi_virtenv/
 activate_this = settings.virtualenv_path + '/bin/activate_this.py'
-execfile(activate_this, dict(__file__=activate_this))
-
-import sys
 
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 
